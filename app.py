9acb76d31ec276c56ae507c68139c7ed450e7e54
Aryan Mistry <mistryaryan2005@gmail.com>
2024-07-28 13:47:03 -0400

    final

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ from flask import Flask, render_template, request
 from record import recorder, stopRecording
 
 
-
 app = Flask(__name__)
 
 
@@ -11,26 +10,22 @@ summaries = []
 
 @app.route("/")
 def home():
-    return render_template(
-        "index.html",
-        summaries=summaries
-    )
+    return render_template("index.html", summaries=summaries)
 
-@app.route('/start_recording', methods=["POST"])
+
+@app.route("/start_recording", methods=["POST"])
 def start_recording():
     global summaries
 
     if request.method == "POST":
-        company = request.form.get('company')
-        participants = request.form.get('participant')
+        company = request.form.get("company")
+        participants = request.form.get("participant")
         summary = recorder()
-        
-        summary = summary.replace('Summary:', '!!!')
-        summary = summary.replace('Key Insights:', '!!!')
-        summary = summary.replace('Next Steps:', '!!!')
-        summary = summary.replace('Questions:', '!!!')
 
-
+        summary = summary.replace("Summary:", "!!!")
+        summary = summary.replace("Key Insights:", "!!!")
+        summary = summary.replace("Next Steps:", "!!!")
+        summary = summary.replace("Questions:", "!!!")
 
         split_summary = summary.split("!!!")
 
@@ -38,17 +33,38 @@ def start_recording():
 
         print(split_summary)
 
-        summaries.append([split_summary[1:], company, participants])
+        summaries.append([len(summaries), split_summary[1:], company, participants])
 
-    
-    return render_template(
-        "index.html"
-    )
+    return render_template("index.html")
 
-@app.route('/stop_recording')
+
+@app.route("/stop_recording")
 def stop_recording():
     stopRecording()
     return home()
 
 
+@app.route("/summary", methods=["GET"])
+def summary():
+    if request.method == "GET":
+        currSummary = summaries[int(request.args["summarynumber"])]
 
+        if len(currSummary[1]) == 4:
+            return render_template(
+                "summary.html",
+                summary=currSummary[1][0],
+                keyinsights=currSummary[1][1],
+                nextsteps=currSummary[1][2],
+                questions=currSummary[1][3],
+            )
+        else:
+            return render_template(
+                "summary.html",
+                summary="",
+                keyinsights="",
+                nextsteps="",
+                questions="",
+            )
+
+    else:
+        return ""
